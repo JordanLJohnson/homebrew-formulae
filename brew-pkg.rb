5f4cfe92e8985e104d137364f78820738f44e719
Jordan Johnson <jordan.luke.johnson@gmail.com>
2024-06-14 10:35:45 +1000

    Update to new namespace and release

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,8 +1,8 @@
 class BrewPkg < Formula
-  desc "Homebrew command for building OS X packages from installed formulae"
-  homepage "https://github.com/timsutton/brew-pkg"
-  url "https://github.com/timsutton/brew-pkg.git", tag:      "v0.1.7",
-                                                   revision: "157dbe8b4696db890517ec3b6bf0c9aa02aedd82"
+  desc "Homebrew command for building macOS packages from installed formulae"
+  homepage "https://github.com/jordanljohnson/brew-pkg"
+  url "https://github.com/jordanljohnson/brew-pkg.git", tag:      "v1.0.0",
+                                                        revision: "a57ddb593f3a1d57d3794d57dbbb00ce0bb34fd7"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
